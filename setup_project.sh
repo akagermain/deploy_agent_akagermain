fdfa2126f9b9920178afa92b9aab878cfd8a5546
akagermain <g.iradukund2@alustudent.com>
2026-06-14 22:16:14 +0200

    Add a script that prompts a user to update attendance thresholds in config.json using sed

--- setup_project.sh
+++ setup_project.sh
@@ -73,6 +73,97 @@ cp "${SCRIPT_DIR}/reports.log"   "${PROJECT_DIR}/reports/reports.log"
 echo "All files placed successfully."
 
 
+# step 6: Dynamic configuration i.e upadate, threshold using sed
+
+echo ""
+read -p "Do you want to update the attendance thresholds? (yes/no): " UPDATE_CONFIG
+
+
+
+if [[ "$UPDATE_CONFIG" == "yes" || "$UPDATE_CONFIG" == "y" ]]; then
+
+
+
+   read -p "Enter new Warning threshold % (default is 75, press Enter to keep): " WARNING_VAL
+
+   if [ -n "$WARNING_VAL" ]; then
+
+      if ! [[ "$WARNING_VAL" =~ ^[0-9]+$ ]] || [ "$WARNING_VAL" -lt 0 ] || [ "WARNING_VAL" -gt 100 ]; then
+          echo "ERROR: Warning threshold must be a whole number between 0 and 100."
+          exit 1
+      fi
+
+       sed -i "s/\"warning\": [0-9]*/\"warning\": ${WARNING_VAL}/" "${PROJECT_DIR}/Helpers/config.json"
+       echo "Warning threshold updated to ${WARNING_VAL}%"
+      else
+        echo "Warning threshold kept at default (75%)."
+    fi
+
+
+
+    read -p "Enter new Failure threshold % (default is 50, press Enter to keep): " FAILURE_VAL
+
+    if [ -n "$FAILURE_VAL" ]; then
+        if ! [[ "$FAILURE_VAL" =~ ^[0-9]+$ ]] || [ "$FAILURE_VAL" -lt 0 ] || [ "$FAILURE_VAL" -gt 100 ]; then
+            echo "ERROR: Failure threshold must be a whole number between 0 and 100."
+            exit 1
+        fi
+
+        sed -i "s/\"failure\": [0-9]*/\"failure\": ${FAILURE_VAL}/" "${PROJECT_DIR}/Helpers/config.json"
+        echo "Failure threshold updated to ${FAILURE_VAL}%"
+    else
+        echo "Failure threshold kept at default (50%)."
+    fi
+
+else
+    echo "Threshold kept at defaults (Warning: 75%, Failure: 50%)."
+fi
+
+
+
+
+# Step 7: Environment validation and health check
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
